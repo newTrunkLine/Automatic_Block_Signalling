dc97134410952a07e2527098389d93ee3ee3824f
Ahasan <46302977+newTrunkLine@users.noreply.github.com>
2023-06-03 17:55:18 -0700

    Added LED function

--- Automatic_Block_Signalling.ino
+++ Automatic_Block_Signalling.ino
@@ -1,32 +1,88 @@
 // Wiring
-const int leftSensorPin 	= A0;
-const int rightSensorPin	= A1;
+const int rightSensorPin	  = A0;
+const int leftSensorPin 	  = A5;
+
+const int leftGreenLEDPin   = 4;
+const int leftYellowLEDPin  = 3;
+const int leftRedLEDPin     = 2;
+const int rightGreenLEDPin  = 11;
+const int rightYellowLEDPin = 12;
+const int rightRedLEDPin    = 13;
 
 // Misc. variables
 const int numSamples = 100;
-const int senseThreshold = 200;
+const int senseThreshold = 250;
 int leftSensorNomVal = 0;
 int rightSensorNomVal = 0;
 
-// Function declarations
-int getNominalSensorVal(int pin);
+enum signalState{
+  stateGreenGreen,
+  stateYellowGreen,
+  stateGreenYellow,
+  stateYellowYellow,
+  stateRedRed,
+}currentState;
 
 void setup() {
   
   Serial.begin(9600);
   while(!Serial){ }
 
+  pinMode(leftGreenLEDPin, OUTPUT);
+  pinMode(leftYellowLEDPin, OUTPUT);
+  pinMode(leftRedLEDPin, OUTPUT);
+  pinMode(rightGreenLEDPin, OUTPUT);
+  pinMode(rightYellowLEDPin, OUTPUT);
+  pinMode(rightRedLEDPin, OUTPUT);
+
+  currentState = stateGreenGreen;
+
   leftSensorNomVal = getNominalSensorVal(leftSensorPin);
   rightSensorNomVal = getNominalSensorVal(rightSensorPin);
+
   Serial.print("Left sensor nominal value: "); Serial.println(leftSensorNomVal);
   Serial.print("Right sensor nominal value: "); Serial.println(rightSensorNomVal);
   delay(1000);
 }
 
 void loop() {
-  bool leftSensorDetect = sensorDetect(leftSensorPin, leftSensorNomVal);
-  bool rightSensorDetect = sensorDetect(rightSensorPin, rightSensorNomVal);
-  Serial.print("Left sensor detect: "); Serial.print(leftSensorDetect ? "TRUE" : "FALSE"); Serial.print(", Right sensor detect: "); Serial.println(rightSensorDetect ? "TRUE" : "FALSE");
+  /*
+  int leftRaw = getSensorRawVal(leftSensorPin);
+  int rightRaw = getSensorRawVal(rightSensorPin);
+  
+  int leftSensorVal = getSensorVal(leftSensorPin, leftSensorNomVal);
+  int rightSensorVal = getSensorVal(rightSensorPin, rightSensorNomVal);
+  
+  //Serial.print("Left raw: "); Serial.print(leftRaw); Serial.print(", Right raw: "); Serial.print(rightRaw); 
+  Serial.print(", Left: "); Serial.print(leftSensorVal); Serial.print(", Right: "); Serial.print(rightSensorVal);
+  */
+
+  bool leftSensorTriggered = isSensorTriggered(leftSensorPin, leftSensorNomVal);
+  bool rightSensorTriggered = isSensorTriggered(rightSensorPin, rightSensorNomVal);
+
+  Serial.print("Left sensor: "); Serial.print(leftSensorTriggered); Serial.print(", Right sensor: "); Serial.print(rightSensorTriggered); Serial.print("Current state: "); Serial.println(currentState);
+  //Serial.print("Left sensor detect: "); Serial.print(leftSensorDetect ? "TRUE" : "FALSE"); Serial.print(", Right sensor detect: "); Serial.println(rightSensorDetect ? "TRUE" : "FALSE");
+
+  switch(currentState){
+    case stateGreenGreen:
+    signalGreenGreen();
+    break;
+
+    case stateYellowGreen:
+    break;
+
+    case stateGreenYellow:
+    break;
+
+    case stateYellowYellow:
+    break;
+
+    case stateRedRed:
+    break;
+  }
+
+  enableLEDs(currentState);
 
   delay(500);
 }
+
--- Functions.ino
+++ Functions.ino
@@ -11,6 +11,53 @@ int getNominalSensorVal(int pin){
   return static_cast<int>(nomSensorVal);
 }
 
-bool sensorDetect(int pin, int nomVal){
-  return ((nomVal - analogRead(pin)) > senseThreshold);
+bool isSensorTriggered(int pin, int nomVal){
+  return (abs(nomVal - analogRead(pin)) > senseThreshold);
+}
+
+void signalGreenGreen(){
+}
+
+void enableLEDs(int state){
+  switch(state){
+    case stateGreenGreen:
+    digitalWrite(leftGreenLEDPin, HIGH);    digitalWrite(rightGreenLEDPin, HIGH);
+  	digitalWrite(leftYellowLEDPin, LOW);    digitalWrite(rightYellowLEDPin, LOW);
+  	digitalWrite(leftRedLEDPin, LOW); 	    digitalWrite(rightRedLEDPin, LOW);
+    break;
+
+    case stateYellowGreen:
+    digitalWrite(leftGreenLEDPin, LOW);     digitalWrite(rightGreenLEDPin, HIGH);
+  	digitalWrite(leftYellowLEDPin, HIGH);   digitalWrite(rightYellowLEDPin, LOW);
+  	digitalWrite(leftRedLEDPin, LOW); 	    digitalWrite(rightRedLEDPin, LOW);
+    break;
+
+    case stateGreenYellow:
+    digitalWrite(leftGreenLEDPin, HIGH);    digitalWrite(rightGreenLEDPin, LOW);
+  	digitalWrite(leftYellowLEDPin, LOW);    digitalWrite(rightYellowLEDPin, HIGH);
+  	digitalWrite(leftRedLEDPin, LOW); 	    digitalWrite(rightRedLEDPin, LOW);
+    break;
+
+    case stateYellowYellow:
+    digitalWrite(leftGreenLEDPin, LOW);     digitalWrite(rightGreenLEDPin, LOW);
+  	digitalWrite(leftYellowLEDPin, HIGH);   digitalWrite(rightYellowLEDPin, HIGH);
+  	digitalWrite(leftRedLEDPin, LOW); 	    digitalWrite(rightRedLEDPin, LOW);
+    break;
+
+    case stateRedRed:
+    digitalWrite(leftGreenLEDPin, LOW);     digitalWrite(rightGreenLEDPin, LOW);
+  	digitalWrite(leftYellowLEDPin, LOW);    digitalWrite(rightYellowLEDPin, LOW);
+  	digitalWrite(leftRedLEDPin, HIGH); 	    digitalWrite(rightRedLEDPin, HIGH);
+    break;
+  }
+}
+
+// Debug Functions
+
+int getSensorRawVal(int pin){
+  return analogRead(pin);
+}
+
+int getSensorVal(int pin, int nomVal){
+  return abs(nomVal - analogRead(pin));
 }
